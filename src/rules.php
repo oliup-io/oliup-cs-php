82355d124ba1cd0dcc82b9ffd3ee4378fa806631
Emile Silas SARE <emile.silas@gmail.com>
2021-08-08 14:06:56 +0100

    updated

--- src/rules.php
+++ src/rules.php
@@ -1,8 +1,21 @@
 <?php
 
 // this is our base rules-set
+// learn more here
+// https://mlocati.github.io/php-cs-fixer-configurator
+
 return [
-	'@PSR12' => true,
-	'array_syntax' => ['syntax' => 'short'],
-	'new_with_braces' => true,
+	'@PSR12'                                        => true,
+	'@PhpCsFixer'                                   => true,
+	'multiline_whitespace_before_semicolons'        => true,
+	'simplified_if_return'                          => true,
+	'global_namespace_import'                       => true,
+	'concat_space'                                  => ['spacing' => 'one'],
+	'phpdoc_tag_casing'                             => true,
+	'binary_operator_spaces'                        => [
+		'operators' => [
+			'='  => 'align',
+			'=>' => 'align',
+		],
+	],
 ];
